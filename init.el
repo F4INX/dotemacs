0356c589804a518b388707d615dfc1e249eab2aa
Hadrien Theveneau <hadrien.theveneau@mailo.com>
2025-07-07 10:48:06 +0200

    Add magit.

--- init.el
+++ init.el
@@ -93,6 +93,12 @@
   (smex-initialize))
 ; ----------------------------------------------------------------------
 
+;; ----------------------------------------------------------------------
+;; Magit
+(use-package magit
+  :ensure t)
+;; ----------------------------------------------------------------------
+
 ; ----------------------------------------------------------------------
 ; Treemacs
 (use-package treemacs
@@ -199,7 +205,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(julia-repl which-key treemacs smex projectile julia-mode company)))
+   '(magit julia-repl which-key treemacs smex projectile julia-mode company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
