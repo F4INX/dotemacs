42d24ec6760967e87ade1be80da4f06fab536d9a
Hadrien Theveneau <hadrien.theveneau@mailo.com>
2025-07-10 10:32:03 +0200

    Auto-update of custom-set-variables.

--- init.el
+++ init.el
@@ -235,8 +235,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(magit julia-repl which-key treemacs smex projectile julia-mode company)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
